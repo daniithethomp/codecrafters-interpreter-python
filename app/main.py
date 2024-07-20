166e96bbb87e8359aa8a9bc58dc4a2ce9d01fe25
Danii Thompson <dithompson1@sheffield.ac.uk>
2024-07-20 23:20:26 +0100

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Token:
     def __init__(self, type, lexeme, literal, line):
         self.type = type
         self.lexeme = lexeme
-        self.literaly = literal
+        self.literal = literal
         self.line = line
     
     def __str__(self):
