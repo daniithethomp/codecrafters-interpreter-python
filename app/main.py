3f081f0d6a2fc0f1645020b05566842ff7da4f1b
Danii Thompson <dithompson1@sheffield.ac.uk>
2024-07-20 22:29:00 +0100

    pass stage

--- app/main.py
+++ app/main.py
@@ -13,30 +13,36 @@ def main():
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
 
+    invalid_tokens = [""]
+
     with open(filename) as file:
         file_contents = file.read()
 
     for c in file_contents:
-        if c == "(":
-            print("LEFT_PAREN ( null")
-        if c == ")":
-            print("RIGHT_PAREN ) null")
-        if c == "{":
-            print("LEFT_BRACE { null")
-        if c == "}":
-            print("RIGHT_BRACE } null")
-        if c == "*":
-            print("STAR * null")
-        if c == ".":
-            print("DOT . null")
-        if c == ",":
-            print("COMMA , null")
-        if c == "+":
-            print("PLUS + null")
-        if c == "-":
-            print("MINUS - null")
-        if c == ";":
-            print("SEMICOLON ; null")
+        match c:
+            case "(":
+                print("LEFT_PAREN ( null")
+            case ")":
+                print("RIGHT_PAREN ) null")
+            case "{":
+                print("LEFT_BRACE { null")
+            case "}":
+                print("RIGHT_BRACE } null")
+            case "*":
+                print("STAR * null")
+            case ".":
+                print("DOT . null")
+            case ",":
+                print("COMMA , null")
+            case "+":
+                print("PLUS + null")
+            case "-":
+                print("MINUS - null")
+            case ";":
+                print("SEMICOLON ; null")
+            case _:
+                print("[line 1] Error: Unexpected character: " + c, file="sys.stderr")
+        
     print("EOF  null") # Placeholder, remove this line when implementing the scanner
 
 
